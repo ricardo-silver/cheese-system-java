3811ee3eba8c6da182b5afc3cda7fb7e50e19764
ricardo-silver <ricardo_override@hotmail.com>
2020-02-13 15:06:30 -0300

    Home Program.java

--- src/application/Program.java
+++ src/application/Program.java
@@ -15,7 +15,7 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		//
+		// inicio
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
